89633e916c99eb94e88b812007a69b3734e842ca
tonyPC <tontruong@mpire.vn>
2020-10-29 17:41:39 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool firstStartUp = true;
+  final fb = FirebaseDatabase.instance;
+  final myController = TextEditingController();
+  final name = "Name";
+  String retrievedName = "";
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+
+  void loadCounterOnFirebase(DataSnapshot fbData) {
+    if (fbData.value != null) {
+      // setState(() {
+      _counter = fbData.value;
+      // });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.reference();
+    // ref.child("counter1").once().then((DataSnapshot data) {
+    //   loadCounterOnFirebase(data);
+    // });
+    ref.child("counter1").once().then((DataSnapshot data) {
+      if (data.value != null && firstStartUp) {
+        setState(() {
+          _counter = data.value;
+          firstStartUp = false;
+          ref.child("Name").once().then((DataSnapshot data1){
+            if(data1.value != null)
+              retrievedName = data1.value;
+          });
+        });
+      }
+    });
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(name),
+                Flexible(child: TextField(controller: myController))
+              ],
+            ),
+            Text('$_counter'),
+            RaisedButton(
+              onPressed: () {
+                ref.child(name).set(myController.text);
+                ref.child("counter1").set(_counter);
+              },
+              child: Text("Submit"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                ref.child("Name").once().then((DataSnapshot data) {
+                  print(data.value);
+                  print(data.key);
+                  setState(() {
+                    retrievedName = data.value;
+                  });
+                });
+              },
+              child: Text("Get"),
+            ),
+            Text('$retrievedName'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
